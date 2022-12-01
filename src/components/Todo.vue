fa997e3feaa1114e4c92c290ad947e19bb5d1dc7
Kotireddy-Kolli <98949858+Kotireddy-Kolli@users.noreply.github.com>
2022-12-01 18:40:29 +0530

    Initial commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div className="header">
+      <h3>Todo App</h3>
+    </div>
+    <div className="user-todo-item">
+      <label for="currentTodo">NewTodo</label>
+      <input type="text" v-model="currentTodo" id="currentTodo" />
+      <button @click="todo.push(currentTodo)">AddNew Todo</button>
+    </div>
+    <h3>Todo Items</h3>
+    <div className="displayTodo">
+      <p v-for="(item, index) in todo" :key="index">{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      currentTodo: "",
+      todo: [],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
